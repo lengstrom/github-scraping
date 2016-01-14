1796d97ca500fe8b6b8c5f9db1f75e50116fb7ce
lengstrom <lengstrom@users.noreply.github.com>
2016-01-14 18:40:39 -0500

    added to get_all_repos

--- get_all_repos.sh
+++ get_all_repos.sh
@@ -8,25 +8,96 @@
 # do forks manually?
 
 #num=1
+source stack.sh
+max=$(($2))
+min=$(($1))
+num=$(($1))
+ghkey=$3
 
-num=`find out -name "out*" -type f | awk -F'[_\.]' '{print $2}' | sort -n -r | head -n 1`
+reset_script() {
+    sleep 5
+    echo "resetting..."
+    ./get_all_repos.sh "$1" "$2" "${ghkey}"
+    echo "Permenantly ending $min -> $max with key $3"
+    exit
+}
 
+num=`find out -name "out*" -type f | awk -F'[_\.]' '{print $2}' | awk -v mini=$min '{if($1==$1+0 && $1>mini)print $1}' | awk -v maxi=$max '{if($1==$1+0 && $1<maxi)print $1}' | sort -r -n | head -n 1`
+
+if [ "g$num" = "g" ];
+then
+    num=$min
+fi
+
+echo "$max, $min, $num"
+
+echo $ghkey
+nn=$((0))
 while true
 do
-    res=`curl -s -u lengstrom:$ghat "https://api.github.com/repositories?since=$num"`
+    cmd=`echo curl -s -u $ghkey "https://api.github.com/repositories?since=$num"`
+    #echo $cmd
+    {
+        res=`$cmd`
+    } || {
+        echo "exiting..."
+        exit
+    }
     if [[ $res == *"rate limit exceeded for"* ]];
     then
-        
+        reset_script
+        echo "Resetting: 0"
+        stack_pop $stackname ghkey
         continue
     fi
-    
-     jq '[.[] | {"id": .id, "name": .full_name, "tree": .trees_url, "fork":.fork}]' > out/out_$num.json
-    num=`cat out/out_$num.json | jq '.[-1] | .id'`
-    if [ $(($num)) -gt $((49615800)) ]
-    then
-        break
-    fi
-    echo "finished ${num}"
-done
 
-#curl -u lengstrom:$ghat "https://api.github.com/search/code?q=filename:bash_history&page=1&per_page=100"
+    if [ "$num" = " " ];
+    then
+        reset_script
+        stack_pop $stackname ghkey
+        num=`find out -name "out*" -type f | awk -F'[_\.]' '{print $2}' | awk -v mini=$min '{if($1==$1+0 && $1>mini)print $1}' | awk -v maxi=$max '{if($1==$1+0 && $1<maxi)print $1}' | sort -r -n | head -n 1`
+        echo "Resetting: 1"
+        num=$(($num+1))
+        continue
+    fi
+
+    if [ $(($num)) -gt $max ];
+    then
+        exit
+    fi
+    
+    {
+        if [ -f out/out_$num.json ]; then
+            echo "File already exists!"
+            nn=$(($nn + 1))
+            if [ $(($nn)) -gt 2 ];
+            then
+                reset_script
+            fi
+            
+            num=`find out -name "out*" -type f | awk -F'[_\.]' '{print $2}' | awk -v mini=$min '{if($1==$1+0 && $1>mini)print $1}' | awk -v maxi=$max '{if($1==$1+0 && $1<maxi)print $1}' | sort -r -n | head -n 1`
+            echo "Resetting: 2"
+            num=$(($num+1))
+        else
+            nn=$((0))
+        fi
+        echo $res | jq '[.[] | {"id": .id, "name": .full_name, "tree": .trees_url, "fork":.fork}]' > out/out_$num.json
+        if [ ! -f out/out_$num.json ]; then
+            echo "File not found!"
+        fi
+        echo "finished ${num}"
+        num=`cat out/out_$num.json | jq '.[-1] | .id'`
+        if [ $(($num)) -gt $((49615800)) ]
+        then
+            break
+        fi
+    } || {
+        reset_script
+        echo "Failed!"
+        stack_pop $stackname ghkey
+        num=`find out -name "out*" -type f | awk -F'[_\.]' '{print $2}' | awk -v mini=$min '{if($1==$1+0 && $1>mini)print $1}' | awk -v maxi=$max '{if($1==$1+0 && $1<maxi)print $1}' | sort -r -n | head -n 1`
+        echo "Resetting: 3"
+        num=$(($num+1))
+    }
+
+done
